740913fea9f7619fe95a459235904ee9bf3540b7
Kushal Das <mail@kushaldas.in>
2017-02-23 22:18:21 +0530

    Adds the first few unit tests

---
+++ utils_test.go
@@ -0,0 +1,32 @@
+package main
+
+import ("testing"
+	"strings")
+
+
+func TestReadCommands(t *testing.T) {
+	data := ReadCommands("./testcommands.txt")
+	inside := []string{"go test", "ls", ""}
+
+	for i, v := range data {
+		if  v != inside[i] {
+			t.Error("Reading commands file failed.", v, inside[i], i)
+		}
+	}
+}
+
+func TestwriteIPinformation(t *testing.T) {
+	m := make(map[string]string)
+	m["ip"] = "127.0.0.1"
+	writeIPinformation(m)
+}
+
+func Testsystem(t *testing.T) {
+	data, err := system("ls ./")
+	if err != nil {
+		t.Error("We have error in running ls command.", data)
+	}
+	if !strings.Contains(data, "main.go") {
+		t.Error("Running ls command failed", data)
+	}
+}
